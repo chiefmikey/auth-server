bbadc5db529e96e91b10590db6a193fed986d455
chiefmikey <wolfemikl@gmail.com>
2021-11-18 21:52:37 -0700

    Update init script to run as ec2-user

--- init.sh
+++ init.sh
@@ -5,15 +5,16 @@ CONTAINER_ALREADY_STARTED="CONTAINER_ALREADY_STARTED_PLACEHOLDER"
 if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
 touch $CONTAINER_ALREADY_STARTED
   echo "-- First container startup --"
-  yum update -y
+  sudo yum update -y
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
   . ~/.nvm/nvm.sh
   nvm install node
+  sudo chown -R ec2-user /home/ec2-user/auth-server
   npm ci
   screen -S node -dm npm start
 else
   echo "-- Not first container startup --"
-  yum update -y
+  sudo yum update -y
   npm ci
   screen -S node -dm npm start
 fi

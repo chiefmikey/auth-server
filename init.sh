740a5627ae16abf1232b4454042a6d165f1bd738
chiefmikey <wolfemikl@gmail.com>
2021-11-18 23:24:58 -0700

    Update init to run as root

--- init.sh
+++ init.sh
@@ -5,17 +5,17 @@ CONTAINER_ALREADY_STARTED="CONTAINER_ALREADY_STARTED_PLACEHOLDER"
 if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
 touch $CONTAINER_ALREADY_STARTED
   echo "-- First container startup --"
-  sudo yum update -y
+  yum update -y
   curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
   . ~/.nvm/nvm.sh
   nvm install node
-  sudo mkdir /home/ec2-user/auth-server/node_modules
-  sudo chown -R ec2-user /home/ec2-user/auth-server
+  mkdir /home/ec2-user/auth-server/node_modules
+  chown -R root /home/ec2-user/auth-server
   npm ci
   screen -S node -dm npm start
 else
   echo "-- Not first container startup --"
-  sudo yum update -y
+  yum update -y
   npm ci
   screen -S node -dm npm start
 fi

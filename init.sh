2ab3477168ca7a415e45853bd33f8f0b49a443d6
chiefmikey <wolfemikl@gmail.com>
2021-11-19 09:04:38 -0700

    Update init script to install nvm and node each update

--- init.sh
+++ init.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
 
 cd /home/ec2-user/auth-server
-CONTAINER_ALREADY_STARTED="CONTAINER_ALREADY_STARTED_PLACEHOLDER"
-if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
-touch $CONTAINER_ALREADY_STARTED
-  echo "-- First container startup --"
-  yum update -y
-  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
-  . ~/.nvm/nvm.sh
-  nvm install node
-  mkdir /home/ec2-user/auth-server/node_modules
-  npm ci
-  screen -S node -dm npm start
-else
-  echo "-- Not first container startup --"
-  yum update -y
-  npm ci
-  screen -S node -dm npm start
-fi
+yum update -y
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
+mkdir /home/ec2-user/auth-server/node_modules
+npm ci
+screen -S node -dm npm start

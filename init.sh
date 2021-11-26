58c19373eed468b945614d99d63e518e2acabe48
chiefmikey <wolfemikl@gmail.com>
2021-11-26 14:54:51 -0700

    Update init script to run full install on each boot

--- init.sh
+++ init.sh
@@ -1,20 +1,10 @@
 #!/bin/bash
 
-INSTANCE_ALREADY_STARTED="INSTANCE_ALREADY_STARTED_PLACEHOLDER"
-if [ ! -e /home/ec2-user/$INSTANCE_ALREADY_STARTED ]; then
-touch /home/ec2-user/$INSTANCE_ALREADY_STARTED
-  echo "-- First instance startup --"
-    cd /home/ec2-user/auth-server
-    yum update -y
-    curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
-    . ~/.nvm/nvm.sh
-    nvm install node
-    mkdir /home/ec2-user/auth-server/node_modules
-    npm ci
-    screen -S node -dm npm start
-else
-  echo "-- Not first instance startup --"
-    yum update -y
-    npm ci
-    screen -S node -dm npm start
-fi
+cd /home/ec2-user/auth-server
+yum update -y
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.0/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install node
+mkdir /home/ec2-user/auth-server/node_modules
+npm ci
+screen -S node -dm npm start

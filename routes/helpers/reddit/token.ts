1983af347666a9d7d69e466ca73ad9beb9e166a5
Mikl Wolfe <wolfemikl@gmail.com>
2022-01-09 10:10:49 -0700

    Update api test

--- routes/helpers/reddit/token.ts
+++ routes/helpers/reddit/token.ts
@@ -2,23 +2,22 @@ import axios from 'axios';
 
 import { id, secret } from './auth';
 
+let response;
+
 const token = async () => {
   try {
-    const response = await axios.get(
-      'https://www.reddit.com/api/v1/access_token',
-      {
-        params: {
-          grant_type: 'client_credentials',
-          user: await id(),
-          password: await secret(),
-        },
+    response = await axios.get('https://www.reddit.com/api/v1/access_token', {
+      params: {
+        grant_type: 'client_credentials',
+        user: await id(),
+        password: await secret(),
       },
-    );
+    });
     if (response && response.data && response.data.access_token) {
       return response.data.access_token;
     }
   } catch {
-    return {};
+    return response.data;
   }
 };
 

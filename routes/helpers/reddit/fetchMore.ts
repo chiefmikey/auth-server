f7326d43d95b82effa80068fe5bf04a3c0591a9a
Mikl Wolfe <wolfemikl@gmail.com>
2022-01-12 05:41:51 -0700

    Add catch to fetchmore await

--- routes/helpers/reddit/fetchMore.ts
+++ routes/helpers/reddit/fetchMore.ts
@@ -1,10 +1,15 @@
 import { Listing, Submission } from 'snoowrap';
 
 const fetchMore = async (response: Listing<Submission> | never[]) => {
-  return await (response as Listing<Submission>).fetchMore({
-    amount: 128,
-    append: false,
-  });
+  try {
+    return await (response as Listing<Submission>).fetchMore({
+      amount: 128,
+      append: false,
+    });
+  } catch (error) {
+    console.error(error);
+    return [];
+  }
 };
 
 export default fetchMore;

2af5e3dd7e37ff070573a639a51263d83e280650
Mikl Wolfe <wolfemikl@gmail.com>
2022-01-12 05:28:40 -0700

    Update types

--- routes/helpers/reddit/fetchMore.ts
+++ routes/helpers/reddit/fetchMore.ts
@@ -1,6 +1,6 @@
 import { Listing, Submission } from 'snoowrap';
 
-const fetchMore = async (response: Listing<Submission> | any[]) => {
+const fetchMore = async (response: Listing<Submission> | never[]) => {
   return await (response as Listing<Submission>).fetchMore({
     amount: 128,
     append: false,

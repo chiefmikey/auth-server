a16058d5e82ef1887002fd9c62c4265e5b046c24
chiefmikey <wolfemikl@gmail.com>
2022-05-19 09:04:45 -0600

    Update init scripts

--- init.sh
+++ init.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-cd /home/ec2-user/auth-server
 yum update -y
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-. ~/.nvm/nvm.sh
-nvm install 16
-mkdir /home/ec2-user/auth-server/node_modules
 npm ci
 screen -S node -dm npm start
--- user-data.sh
+++ user-data.sh
@@ -2,5 +2,10 @@
 
 yum install -y wget
 wget -O ~/codedeploy-amazon.sh https://raw.githubusercontent.com/chiefmikey/tales-from-the-script/main/codedeploy/codedeploy-amazon.sh
-sudo chmod +x ~/codedeploy-amazon.sh
-sudo ~/codedeploy-amazon.sh
+chmod +x ~/codedeploy-amazon.sh
+~/codedeploy-amazon.sh
+cd /home/ec2-user/auth-server
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+. ~/.nvm/nvm.sh
+nvm install 16
+mkdir /home/ec2-user/auth-server/node_modules

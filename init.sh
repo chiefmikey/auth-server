7d0f216fce11d43178d9b6b8e1fa423bd83c0526
chiefmikey <wolfemikl@gmail.com>
2022-05-18 22:38:01 -0600

    Update init script

--- init.sh
+++ init.sh
@@ -3,6 +3,7 @@
 cd /home/ec2-user/auth-server
 yum update -y
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+source ~/.bashrc
 . ~/.nvm/nvm.sh
 nvm install node
 mkdir /home/ec2-user/auth-server/node_modules

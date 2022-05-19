ab09ab40b07d30be9acbc0a0e462ab7c996009b2
chiefmikey <wolfemikl@gmail.com>
2022-05-19 07:20:48 -0600

    Update init script

--- init.sh
+++ init.sh
@@ -3,9 +3,8 @@
 cd /home/ec2-user/auth-server
 yum update -y
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-source ~/.bashrc
 . ~/.nvm/nvm.sh
-nvm install node
+nvm install 16
 mkdir /home/ec2-user/auth-server/node_modules
 npm ci
 screen -S node -dm npm start

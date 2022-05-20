eb914e55384d3d3dc9e15124f75d6a3620504433
chiefmikey <wolfemikl@gmail.com>
2022-05-19 20:16:20 -0600

    Update init scripts

--- init.sh
+++ init.sh
@@ -1,20 +1,5 @@
 #!/bin/bash
 
-CONTAINER_ALREADY_STARTED="CONTAINER_ALREADY_STARTED_PLACEHOLDER"
-if [ ! -e $CONTAINER_ALREADY_STARTED ]; then
-touch $CONTAINER_ALREADY_STARTED
-  echo "-- First container startup --"
-  yum update -y
-  cd /home/ec2-user/auth-server
-  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
-  . ~/.nvm/nvm.sh
-  nvm install 16
-  mkdir /home/ec2-user/auth-server/node_modules
-  npm ci
-  screen -S node -dm npm start
-else
-  echo "-- Not first container startup --"
-  yum update -y
-  npm ci
-  screen -S node -dm npm start
-fi
+yum update -y
+npm ci
+screen -S node -dm npm start
--- user-data.sh
+++ user-data.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
+yum update -y
 yum install -y wget
 wget -O ~/codedeploy-amazon.sh https://raw.githubusercontent.com/chiefmikey/tales-from-the-script/main/codedeploy/codedeploy-amazon.sh
 chmod +x ~/codedeploy-amazon.sh
 ~/codedeploy-amazon.sh
+curl -fsSL https://rpm.nodesource.com/setup_lts.x | bash -
+yum install -y nodejs

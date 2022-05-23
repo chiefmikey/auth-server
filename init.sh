8902d84d3087eb86c9d0cc18a49cf7743c826266
chiefmikey <wolfemikl@gmail.com>
2022-05-23 14:24:05 -0600

    Update init scripts

--- init.sh
+++ init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-yum update -y
-cd /home/ec2-user/auth-server
+yum update -y && yum upgrade -y
+cd auth-server
 npm ci
 screen -Sdm node npm start
--- user-data.sh
+++ user-data.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-yum update -y
-yum install -y wget curl
+yum update -y && yum upgrade -y
+yum install -y wget curl git
 wget -O ~/codedeploy-amazon.sh https://raw.githubusercontent.com/chiefmikey/tales-from-the-script/main/codedeploy/codedeploy-amazon.sh
 chmod +x ~/codedeploy-amazon.sh
 ~/codedeploy-amazon.sh
 curl -fsSL https://rpm.nodesource.com/setup_lts.x | bash -
 yum install -y nodejs
-mkdir /home/ec2-user/auth-server
-mkdir /home/ec2-user/auth-server/node_modules
-cd /home/ec2-user/auth-server
+git clone https://github.com/chiefmikey/auth-server.git
+cd auth-server
 npm ci
 screen -Sdm node npm start

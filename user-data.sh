8309e337a621227320f9f77bf97e6498e122d062
chiefmikey <wolfemikl@gmail.com>
2022-08-11 15:34:04 -0600

    Update user-data

--- user-data.sh
+++ user-data.sh
@@ -2,11 +2,13 @@
 
 yum update -y && yum upgrade -y
 yum install -y wget curl git
-wget -O ~/codedeploy-amazon.sh https://raw.githubusercontent.com/chiefmikey/tales-from-the-script/main/codedeploy/codedeploy-amazon.sh
-chmod +x ~/codedeploy-amazon.sh
-~/codedeploy-amazon.sh
+wget -O ~/codedeploy-al2.sh https://raw.githubusercontent.com/chiefmikey/linux-codedeploy/main/linux-codedeploy-al2.sh
+sudo chmod +x ~/codedeploy-al2.sh
+sudo ~/codedeploy-al2.sh
 curl -fsSL https://rpm.nodesource.com/setup_lts.x | bash -
 yum install -y nodejs
+rm -r install
+rm -r codedeploy-al2.sh
 git clone https://github.com/chiefmikey/auth-server.git
 cd /auth-server
 npm ci

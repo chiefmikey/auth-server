182fa8f76719aeff8124d75410311840653ee2e7
chiefmikey <wolfemikl@gmail.com>
2022-08-11 21:34:49 -0600

    Clean up repos call

--- routes/helpers/github/repos.ts
+++ routes/helpers/github/repos.ts
@@ -4,11 +4,10 @@ let octokit: Octokit;
 
 const repos = async (username: string, token: string) => {
   try {
-    // const allRepos = [];
     if (!octokit) {
       octokit = new Octokit({ auth: token });
     }
-    const response = await octokit.paginate(
+    return await octokit.paginate(
       octokit.rest.repos.listForUser,
       {
         username,
@@ -16,19 +15,9 @@ const repos = async (username: string, token: string) => {
       },
       (response) =>
         response.data.map((repo) => {
-          console.log('indy responsey', repo);
           return repo.name;
         }),
     );
-    console.log('full res', response);
-    // for await (const { data } of response) {
-    //   for (const repo of data) {
-    //     allRepos.push(repo);
-    //   }
-    // }
-    // return allRepos;
-    // console.log('repos', response);
-    return response;
   } catch {
     return [];
   }

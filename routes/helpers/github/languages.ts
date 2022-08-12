3ff262e773c0241bd30e65eccd1e939ddbb6984b
chiefmikey <wolfemikl@gmail.com>
2022-08-11 22:37:37 -0600

    Cleanup languages call

--- routes/helpers/github/languages.ts
+++ routes/helpers/github/languages.ts
@@ -4,7 +4,6 @@ let octokit: Octokit;
 
 const fetchLanguage = async (owner: string, repo: string, token: string) => {
   try {
-    // const allLangs = [];
     if (!octokit) {
       octokit = new Octokit({ auth: token });
     }

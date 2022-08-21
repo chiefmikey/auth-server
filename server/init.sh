72ac9aa950cb2a9d2128ffb47419e874b67b3a16
chiefmikey <wolfemikl@gmail.com>
2022-08-21 12:58:34 -0600

    Update init scripts

--- server/init.sh
+++ server/init.sh
@@ -4,6 +4,6 @@ yum update -y && yum upgrade -y
 cd /home/ec2-user/auth-server
 npm ci
 
-[ -f "/home/ec2-user/gitlang/gitlang" ] && [ -f "/home/ec2-user/vm3000/vm3000" ] && screen -Sdm node npm run start:server
-[ -f "/home/ec2-user/gitlang/gitlang" ] && [ ! -f "/home/ec2-user/vm3000/vm3000" ] && screen -Sdm node npm run start:gitlang
-[ ! -f "/home/ec2-user/gitlang/gitlang" ] && [ -f "/home/ec2-user/vm3000/vm3000" ] && screen -Sdm node npm run start:vm3000
+[ -f "/home/ec2-user/gitlang/gitlang" ] && [ -f "/home/ec2-user/vm3000/vm3000" ] && screen -Sdm node npm run server:start
+[ -f "/home/ec2-user/gitlang/gitlang" ] && [ ! -f "/home/ec2-user/vm3000/vm3000" ] && screen -Sdm node npm run server:gitlang
+[ ! -f "/home/ec2-user/gitlang/gitlang" ] && [ -f "/home/ec2-user/vm3000/vm3000" ] && screen -Sdm node npm run server:vm3000

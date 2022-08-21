4099eeb1313bc6d22beb1aa8c927798969185aee
chiefmikey <wolfemikl@gmail.com>
2022-08-21 13:27:11 -0600

    Update init dirs

--- server/init.sh
+++ server/init.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 yum update -y && yum upgrade -y
-cd /home/ec2-user/server
+cd /home/ec2-user/auth/server
 npm ci
 
-[ -f "/home/ec2-user/gitlang/gitlang.ts" ] && [ -f "/home/ec2-user/vm3000/vm3000.ts" ] && screen -Sdm node npm run server:start
-[ -f "/home/ec2-user/gitlang/gitlang.ts" ] && [ ! -f "/home/ec2-user/vm3000/vm3000.ts" ] && screen -Sdm node npm run server:gitlang
-[ ! -f "/home/ec2-user/gitlang/gitlang.ts" ] && [ -f "/home/ec2-user/vm3000/vm3000.ts" ] && screen -Sdm node npm run server:vm3000
+[ -f "/home/ec2-user/auth/gitlang/gitlang.ts" ] && [ -f "/home/ec2-user/auth/vm3000/vm3000.ts" ] && screen -Sdm node npm run server:start
+[ -f "/home/ec2-user/auth/gitlang/gitlang.ts" ] && [ ! -f "/home/ec2-user/auth/vm3000/vm3000.ts" ] && screen -Sdm node npm run server:gitlang
+[ ! -f "/home/ec2-user/auth/gitlang/gitlang.ts" ] && [ -f "/home/ec2-user/auth/vm3000/vm3000.ts" ] && screen -Sdm node npm run server:vm3000

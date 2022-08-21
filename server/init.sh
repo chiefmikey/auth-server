1b386f2d0cd73d0422bf91a88f0b3e62ccc2df92
chiefmikey <wolfemikl@gmail.com>
2022-08-21 14:27:52 -0600

    Update server dir

--- server/init.sh
+++ server/init.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 yum update -y && yum upgrade -y
-cd /home/ec2-user/auth/server
+cd /home/ec2-user/server
 npm ci
 
-[ -f "/home/ec2-user/auth/gitlang/gitlang.ts" ] && [ -f "/home/ec2-user/auth/vm3000/vm3000.ts" ] && screen -Sdm node npm run server:start
-[ -f "/home/ec2-user/auth/gitlang/gitlang.ts" ] && [ ! -f "/home/ec2-user/auth/vm3000/vm3000.ts" ] && screen -Sdm node npm run server:gitlang
-[ ! -f "/home/ec2-user/auth/gitlang/gitlang.ts" ] && [ -f "/home/ec2-user/auth/vm3000/vm3000.ts" ] && screen -Sdm node npm run server:vm3000
+[ -f "/home/ec2-user/server/gitlang/gitlang.ts" ] && [ -f "/home/ec2-user/server/vm3000/vm3000.ts" ] && screen -Sdm node npm run server:start
+[ -f "/home/ec2-user/server/gitlang/gitlang.ts" ] && [ ! -f "/home/ec2-user/server/vm3000/vm3000.ts" ] && screen -Sdm node npm run server:gitlang
+[ ! -f "/home/ec2-user/server/gitlang/gitlang.ts" ] && [ -f "/home/ec2-user/server/vm3000/vm3000.ts" ] && screen -Sdm node npm run server:vm3000
 
-screen -Sdm cron /home/ec2-user/auth/server/cron.sh
+screen -Sdm cron /home/ec2-user/server/cron.sh

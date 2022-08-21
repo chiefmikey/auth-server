c8a624c70e6366e560938d087f2bef41d6e70000
chiefmikey <wolfemikl@gmail.com>
2022-08-21 13:47:28 -0600

    Increment count

--- server/init.sh
+++ server/init.sh
@@ -11,6 +11,7 @@ npm ci
 COUNT=0
 while [ ! -f "/home/ec2-user/auth/gitlang/gitlang.ts" ] || [ ! -f "/home/ec2-user/auth/vm3000/vm3000.ts" ]; do
   [ ${COUNT} -eq 0 ] && echo "Trigger app deploy workflows"
+  COUNT+=1
   echo "Updating..."
   sleep 60
 done

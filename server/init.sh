92f0085e7733dbf93fbc2b9eddc726ceb5bb7c7b
chiefmikey <wolfemikl@gmail.com>
2022-08-21 12:58:52 -0600

    Update init server dir

--- server/init.sh
+++ server/init.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 yum update -y && yum upgrade -y
-cd /home/ec2-user/auth-server
+cd /home/ec2-user/server
 npm ci
 
 [ -f "/home/ec2-user/gitlang/gitlang" ] && [ -f "/home/ec2-user/vm3000/vm3000" ] && screen -Sdm node npm run server:start

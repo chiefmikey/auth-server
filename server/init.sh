06331d87433c32e7e1ad237ce15477ae93357c71
chiefmikey <wolfemikl@gmail.com>
2022-08-21 13:46:37 -0600

    Add deploy cron

--- server/init.sh
+++ server/init.sh
@@ -7,3 +7,10 @@ npm ci
 [ -f "/home/ec2-user/auth/gitlang/gitlang.ts" ] && [ -f "/home/ec2-user/auth/vm3000/vm3000.ts" ] && screen -Sdm node npm run server:start
 [ -f "/home/ec2-user/auth/gitlang/gitlang.ts" ] && [ ! -f "/home/ec2-user/auth/vm3000/vm3000.ts" ] && screen -Sdm node npm run server:gitlang
 [ ! -f "/home/ec2-user/auth/gitlang/gitlang.ts" ] && [ -f "/home/ec2-user/auth/vm3000/vm3000.ts" ] && screen -Sdm node npm run server:vm3000
+
+COUNT=0
+while [ ! -f "/home/ec2-user/auth/gitlang/gitlang.ts" ] || [ ! -f "/home/ec2-user/auth/vm3000/vm3000.ts" ]; do
+  [ ${COUNT} -eq 0 ] && echo "Trigger app deploy workflows"
+  echo "Updating..."
+  sleep 60
+done

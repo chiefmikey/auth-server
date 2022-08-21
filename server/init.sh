d8d4d15d00c853b8e15b0e2cc10196a0afdbcf0f
chiefmikey <wolfemikl@gmail.com>
2022-08-21 13:00:38 -0600

    Update init conditions file extensions

--- server/init.sh
+++ server/init.sh
@@ -4,6 +4,6 @@ yum update -y && yum upgrade -y
 cd /home/ec2-user/server
 npm ci
 
-[ -f "/home/ec2-user/gitlang/gitlang" ] && [ -f "/home/ec2-user/vm3000/vm3000" ] && screen -Sdm node npm run server:start
-[ -f "/home/ec2-user/gitlang/gitlang" ] && [ ! -f "/home/ec2-user/vm3000/vm3000" ] && screen -Sdm node npm run server:gitlang
-[ ! -f "/home/ec2-user/gitlang/gitlang" ] && [ -f "/home/ec2-user/vm3000/vm3000" ] && screen -Sdm node npm run server:vm3000
+[ -f "/home/ec2-user/gitlang/gitlang.ts" ] && [ -f "/home/ec2-user/vm3000/vm3000.ts" ] && screen -Sdm node npm run server:start
+[ -f "/home/ec2-user/gitlang/gitlang.ts" ] && [ ! -f "/home/ec2-user/vm3000/vm3000.ts" ] && screen -Sdm node npm run server:gitlang
+[ ! -f "/home/ec2-user/gitlang/gitlang.ts" ] && [ -f "/home/ec2-user/vm3000/vm3000.ts" ] && screen -Sdm node npm run server:vm3000

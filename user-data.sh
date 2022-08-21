1d0b1fd080ff8f495fd1eeb5da5e326079d63e3b
chiefmikey <wolfemikl@gmail.com>
2022-08-21 13:46:46 -0600

    Update user-data script

--- user-data.sh
+++ user-data.sh
@@ -2,14 +2,11 @@
 
 yum update -y && yum upgrade -y
 yum install -y wget curl git
-wget -O ~/codedeploy-al2.sh https://raw.githubusercontent.com/chiefmikey/linux-codedeploy/main/linux-codedeploy-al2.sh
-sudo chmod +x ~/codedeploy-al2.sh
-sudo ~/codedeploy-al2.sh
+wget -O /home/ec2-user/codedeploy-al2.sh https://raw.githubusercontent.com/chiefmikey/linux-codedeploy/main/linux-codedeploy-al2.sh
+chmod +x /home/ec2-user/codedeploy-al2.sh
+/home/ec2-user/codedeploy-al2.sh
 curl -fsSL https://rpm.nodesource.com/setup_lts.x | bash -
 yum install -y nodejs
-rm -r install
-rm -r codedeploy-al2.sh
-git clone https://github.com/chiefmikey/auth-server.git
-cd /auth-server
-npm ci
-screen -Sdm node npm start
+rm -r /home/ec2-user/install
+rm -r /home/ec2-user/codedeploy-al2.sh
+# Trigger init deploy workflows

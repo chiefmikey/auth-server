3756e286978bc13947be77e7bbaa69adf4036936
chiefmikey <wolfemikl@gmail.com>
2022-08-21 14:33:36 -0600

    Update cron condition and permissions

--- server/init.sh
+++ server/init.sh
@@ -8,4 +8,7 @@ npm ci
 [ -f "/home/ec2-user/server/gitlang/gitlang.ts" ] && [ ! -f "/home/ec2-user/server/vm3000/vm3000.ts" ] && screen -Sdm node npm run server:gitlang
 [ ! -f "/home/ec2-user/server/gitlang/gitlang.ts" ] && [ -f "/home/ec2-user/server/vm3000/vm3000.ts" ] && screen -Sdm node npm run server:vm3000
 
-screen -Sdm cron /home/ec2-user/server/cron.sh
+if [ ! -f "/home/ec2-user/server/gitlang/gitlang.ts" ] || [ ! -f "/home/ec2-user/server/vm3000/vm3000.ts" ]; then
+  chmod +x /home/ec2-user/server/cron.sh
+  screen -Sdm cron /home/ec2-user/server/cron.sh
+fi

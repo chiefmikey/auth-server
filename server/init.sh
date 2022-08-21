1c71d679607f390bcbdca3858bf51c0722384797
chiefmikey <wolfemikl@gmail.com>
2022-08-21 12:56:50 -0600

    Update init conditions

--- server/init.sh
+++ server/init.sh
@@ -3,4 +3,7 @@
 yum update -y && yum upgrade -y
 cd /home/ec2-user/auth-server
 npm ci
-screen -Sdm node npm run start
+
+[ -f "/home/ec2-user/gitlang/gitlang" ] && [ -f "/home/ec2-user/vm3000/vm3000" ] && screen -Sdm node npm run start:server
+[ -f "/home/ec2-user/gitlang/gitlang" ] && [ ! -f "/home/ec2-user/vm3000/vm3000" ] && screen -Sdm node npm run start:gitlang
+[ ! -f "/home/ec2-user/gitlang/gitlang" ] && [ -f "/home/ec2-user/vm3000/vm3000" ] && screen -Sdm node npm run start:vm3000

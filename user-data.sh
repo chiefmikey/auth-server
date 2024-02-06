639fe0e5704aed74f7739f803902ad6b2da06937
Mikl Wolfe <wolfemikl@gmail.com>
2024-02-06 10:50:15 -0700

    fix(package): update user data

--- user-data.sh
+++ user-data.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 yum update -y && yum upgrade -y
-yum install -y wget curl git
-wget -O /home/ec2-user/codedeploy-al2.sh https://raw.githubusercontent.com/chiefmikey/linux-codedeploy/main/linux-codedeploy-al2.sh
+yum install -y wget git
+wget -O /home/ec2-user/codedeploy-al2.sh https://raw.githubusercontent.com/chiefmikey/tales-from-the-script/main/linux/codedeploy/linux-codedeploy-al2.sh
 chmod +x /home/ec2-user/codedeploy-al2.sh
 /home/ec2-user/codedeploy-al2.sh
 curl -fsSL https://rpm.nodesource.com/setup_lts.x | bash -
